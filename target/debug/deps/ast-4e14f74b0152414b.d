ea17c4c2160d4302691c79a63603c9f1ece5586a
heru h day <heru.hday@gmail.com>
2026-04-02 16:55:14 +0700

    resolve conflict and remove target from repo

--- target/debug/deps/ast-4e14f74b0152414b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Heruh\source\qjs\target\debug\deps\ast-4e14f74b0152414b.d: core\ast\src\main.rs
-
-C:\Users\Heruh\source\qjs\target\debug\deps\libast-4e14f74b0152414b.rmeta: core\ast\src\main.rs
-
-core\ast\src\main.rs:
